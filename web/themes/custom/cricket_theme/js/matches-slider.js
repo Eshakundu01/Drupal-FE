d21bc8a7261c9b9bcc4bd9021b4d303d35416694
Esha Kundu <esha.kundu@innoraft.com>
2023-10-16 19:40:30 +0530

    FT2023-0000: Add part 2 task.

--- web/themes/custom/cricket_theme/js/matches-slider.js
+++ web/themes/custom/cricket_theme/js/matches-slider.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * World Cup cricket slider behaviours.
+ * World Cup slider behaviours.
  */
 
 (function ($, Drupal) {
@@ -10,11 +10,11 @@
         $(element).find('.view-content').slick({
           dots: true,
           infinite: 0,
-          prevArrow: '<i class="bi bi-chevron-left"></i>',
-          nextArrow: '<i class="bi bi-chevron-right"></i>',
           autoplay: true,
           pauseOnHover: false,
           pauseOnFocus: false,
+          prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='bi bi-chevron-left' aria-hidden='true'></i></button>",
+          nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='bi bi-chevron-right' aria-hidden='true'></i></button>",
           fade: true,
           cssEase: 'linear',
           speed: 500,
